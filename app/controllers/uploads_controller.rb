6a18f1b0e54cd275130829872eba03571a198195
Derevianko Liubov <dereviankoliubov@gmail.com>
2015-12-10 21:12:03 +0200

    Added delayed_job gem and config it

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,16 @@
+class UploadsController < ApplicationController
+  def index
+  end
+
+  def format
+    @doc = Document.create(name: 'Invisible')
+    # @doc.delay.format
+    @doc.format
+  end
+
+  def status
+    @doc = Document.find(params[:document_id])
+    render json: { id: @doc.id, imported: @doc.imported }
+  end
+
+end
---
+++ app/models/document.rb
@@ -0,0 +1,7 @@
+class Document < ActiveRecord::Base
+  def format
+    sleep 5
+    update_column :imported, true
+  end
+  handle_asynchronously :format
+end
